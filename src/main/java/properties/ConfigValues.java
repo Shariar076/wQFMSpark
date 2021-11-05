907391580bf5bb62badb37408a1b677fdd45aeae
Himel <shariar.kabir@cellosco.pe>
2021-11-05 16:23:09 +0600

    spark version reverted tp 3.1.2

--- src/main/java/properties/ConfigValues.java
+++ src/main/java/properties/ConfigValues.java
@@ -7,7 +7,7 @@ import phylonet.tree.model.sti.STITree;
 
 public class ConfigValues {
     public static final String TRIPLETS_SODA_PATH = "./triplets.soda2103";
-    public static final String INPUT_FILE_NAME = "input/gtree_11tax_est_5genes_R1.tre"; //DefaultConfigs.INPUT_FILE_NAME_WQRTS_DEFAULT; // "input_files/weighted_quartets_avian_biological_dataset";
+    public static final String INPUT_FILE_NAME = "input/37_taxon_all_gt.tre"; //DefaultConfigs.INPUT_FILE_NAME_WQRTS_DEFAULT; // "input_files/weighted_quartets_avian_biological_dataset";
     public static final String OUTPUT_FILE_NAME = "output/output-gt.tree";
     public static final String HDFS_PATH = "hdfs://localhost:9000/user/himel/";
     public static final int TAXA_PER_PARTITION = 8;
