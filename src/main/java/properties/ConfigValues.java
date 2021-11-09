70dbdecfb2f55cabc25a5ab5ad12abf4a306d670
kab076 <kab076@node0.wqfmspark.ugthesis-pg0.utah.cloudlab.us>
2021-11-09 04:22:21 -0700

    local change

--- src/main/java/properties/ConfigValues.java
+++ src/main/java/properties/ConfigValues.java
@@ -7,11 +7,11 @@ import phylonet.tree.model.sti.STITree;
 
 public class ConfigValues {
     public static final String TRIPLETS_SODA_PATH = "./triplets.soda2103";
-    public static String INPUT_MODE = "gene-trees"; //wqrt, gene-trees
-    public static String INPUT_FILE_NAME = "input/37_taxon_all_gt.tre"; //"input/37_taxa.wqrts";
+    public static String INPUT_MODE = "wqrt"; //wqrt, gene-trees
+    public static String INPUT_FILE_NAME = "input/101_taxa_01.true.wqrts"; //"input/37_taxa.wqrts";
     public static String OUTPUT_FILE_NAME = "output/output-gt.tree";
-    public static final String HDFS = "hdfs://localhost:9000";
-    public static final String HDFS_USER = "himel";
+    public static final String HDFS = "hdfs://ms0819.utah.cloudlab.us:9000";
+    public static final String HDFS_USER = "kab076";
     public static final String HDFS_PATH = HDFS + "/user/" + HDFS_USER + "/";
     public static final int TAXA_PER_PARTITION = 36;
 
@@ -28,8 +28,8 @@ public class ConfigValues {
 
         SparkConf conf = new SparkConf().setAppName("wQFMSpark")
                 // .setJars(new String[]{System.getProperty("user.dir") + "/target/wQFMSpark-1.0-SNAPSHOT-jar-with-dependencies.jar"})
-                .setMaster("local")
-                // .setMaster("spark://doer-ThinkPad-T460s:7077")
+                // .setMaster("local")
+                .setMaster("spark://node0:7077")
                 ;
         // conf.registerKryoClasses(new Class<?>[]{ STITree.class});
         JavaSparkContext jsc = new JavaSparkContext(conf);
