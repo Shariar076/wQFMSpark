a54597f8d0ae7a07850d1c4a7da71d0540a1dc75
Himel <shariar.kabir@cellosco.pe>
2021-11-17 14:20:27 +0600

    update

--- scripts/spark-submit.sh
+++ scripts/spark-submit.sh
@@ -1,1 +1,5 @@
-~/.spark/bin/spark-submit --conf "spark.executor.memory=40g" --files ./triplets.soda2103  target/wQFMSpark-1.0-SNAPSHOT-jar-with-dependencies.jar
+/dfs/.spark/bin/spark-submit \
+  --conf "spark.executor.memory=20g" \
+  --conf "spark.driver.memory=20g" \
+  --files ./triplets.soda2103 ./target/wQFMSpark-1.0-SNAPSHOT-jar-with-dependencies.jar
+#  --conf "spark.task.cpus=4" \
